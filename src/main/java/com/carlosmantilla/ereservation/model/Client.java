5e15d7a0aae815ef338f1926b72da947b8676e63
carlosmantilla23 <carlos.mantilla.jaimes@gmail.com>
2019-03-26 22:52:37 -0500

    Primer pojo con lombok (@data annotation)

---
+++ src/main/java/com/carlosmantilla/ereservation/model/Client.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.carlosmantilla.ereservation.model;
+
+import lombok.Data;
+
+/**
+ * Clase que representa la tabla Client
+ * @author carlosmantilla
+ *
+ */
+@Data
+public class Client {
+	
+	String clientName;
+	String clientSurname;
+	
+	
+
+}
